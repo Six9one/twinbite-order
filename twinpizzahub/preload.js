d5eec818675561a7835123e8f9c20cc7bd290dbf
Six9one <92888940+Six9one@users.noreply.github.com>
2026-06-29 04:10:08 +0200

    fix: convert preloadPath to file:// URL for webview compatibility on Windows

--- twinpizzahub/preload.js
+++ twinpizzahub/preload.js
@@ -51,7 +51,7 @@ contextBridge.exposeInMainWorld('twinHub', {
     return () => ipcRenderer.removeListener('freebox-call', listener);
   },
 
-  preloadPath: path.join(__dirname, 'preload.js'),
+  preloadPath: 'file://' + path.join(__dirname, 'preload.js').replace(/\\/g, '/'),
   platform: typeof process !== 'undefined' ? process.platform : 'win32',
   appUrl: (typeof process !== 'undefined' && process.argv && process.argv.includes('--dev')) ? 'http://localhost:8080' : 'http://localhost:3456',
 });
